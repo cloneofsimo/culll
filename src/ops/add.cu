a6d3e2204f21e9d613f993515d3a84f05c8172e5
SimoRyu <cloneofsimo@korea.ac.kr>
2022-10-03 09:23:09 +0000

    refactor: add

--- src/ops/add.cu
+++ src/ops/add.cu
@@ -1,5 +1,9 @@
 
-#include <cuda_runtime.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+#include <gpu_utils.h>
+
 
 using lint = unsigned int;
 
@@ -38,4 +42,72 @@ __global__ void batchLongTensorAdd(
     }
     output_data[pos + i] = sum;
   }
+}
+
+
+
+void batchLongTensorAddWrapper(
+  pybind11::array_t<lint> batched_data_a, pybind11::array_t<lint> batched_data_b, pybind11::array_t<lint> output_data, lint B, lint N, lint M, lint n)
+{
+  pybind11::buffer_info ha = batched_data_a.request();
+  pybind11::buffer_info hb = batched_data_b.request();
+  pybind11::buffer_info hc = output_data.request();
+
+  if (ha.ndim != 4)
+  {
+    std::stringstream strstr;
+    strstr << "ha.ndim != 4" << std::endl;
+    strstr << "ha.ndim: " << ha.ndim << std::endl;
+    throw std::runtime_error(strstr.str());
+  }
+
+  if (hb.ndim != 4)
+  {
+    std::stringstream strstr;
+    strstr << "hb.ndim != 4" << std::endl;
+    strstr << "hb.ndim: " << hb.ndim << std::endl;
+    throw std::runtime_error(strstr.str());
+  }
+
+  if (hc.ndim != 4)
+  {
+    std::stringstream strstr;
+    strstr << "hc.ndim != 4" << std::endl;
+    strstr << "hc.ndim: " << hc.ndim << std::endl;
+    throw std::runtime_error(strstr.str());
+  }
+
+  std::cout << "ha.shape[0]: " << ha.shape[0] << std::endl;
+  std::cout << "ha.shape[1]: " << ha.shape[1] << std::endl;
+  std::cout << "ha.shape[2]: " << ha.shape[2] << std::endl;
+  std::cout << "ha.shape[3]: " << ha.shape[3] << std::endl;
+
+  std::cout << "hb.shape[0]: " << hb.shape[0] << std::endl;
+  std::cout << "hb.shape[1]: " << hb.shape[1] << std::endl;
+  std::cout << "hb.shape[2]: " << hb.shape[2] << std::endl;
+  std::cout << "hb.shape[3]: " << hb.shape[3] << std::endl;
+
+  std::cout << "ha size: " << ha.size * sizeof(lint) << std::endl;
+
+  lint* gpu_ptr_a;
+  lint* gpu_ptr_b;
+  lint* gpu_ptr_c;
+
+  gpuErrchk(cudaMalloc(&gpu_ptr_a, ha.size * sizeof(lint)));
+  gpuErrchk(cudaMalloc(&gpu_ptr_b, hb.size * sizeof(lint)));
+  gpuErrchk(cudaMalloc(&gpu_ptr_c, hc.size * sizeof(lint)));
+
+  gpuErrchk(cudaMemcpy(gpu_ptr_a, ha.ptr, ha.size * sizeof(lint), cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(gpu_ptr_b, hb.ptr, hb.size * sizeof(lint), cudaMemcpyHostToDevice));
+
+  dim3 dimBlock(1, 1, 1);
+  dim3 dimGrid(B, N, M);
+
+  batchLongTensorAdd << <dimGrid, dimBlock >> > (gpu_ptr_a, gpu_ptr_b, gpu_ptr_c, B, n, N, M);
+  lint* ptr = reinterpret_cast<lint*>(hc.ptr);
+  gpuErrchk(cudaMemcpy(ptr, gpu_ptr_c, hc.size * sizeof(lint), cudaMemcpyDeviceToHost));
+
+  cudaFree(gpu_ptr_a);
+  cudaFree(gpu_ptr_b);
+  cudaFree(gpu_ptr_c);
 }
